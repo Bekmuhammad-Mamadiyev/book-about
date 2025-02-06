c3f4d30bdd71134c6ff4051b5df0b299afe887e1
Bekmuhammad-Mamadiyev <bekmuhammadmamadiyev90@gmail.com>
2025-02-06 17:07:29 +0500

    upgrade

--- keyboards.py
+++ keyboards.py
@@ -1,16 +1,19 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def recent_books_list(books_data):
-    builder = InlineKeyboardBuilder()
-    for i, book in enumerate(books_data):
-        builder.button(text=f"{i+1}", callback_data=f"id_{book['id']}")
-    builder.adjust(5)
-    return builder.as_markup() if builder.buttons else InlineKeyboardMarkup(inline_keyboard=[])
+    recent_books = InlineKeyboardMarkup(row_width=5)
+    inline_buttons = [InlineKeyboardButton(text=f"{i+1}", callback_data=f"id_{books_data[i]['id']}")
+                      for i in range(len(books_data))]
+    recent_books.add(*inline_buttons)
+    return recent_books
+
 
 def book_keyboard(url, download):
-    builder = InlineKeyboardBuilder()
-    builder.button(text="↗️ Url", url=url)
-    builder.button(text="📥 Download", url=download)
-    builder.adjust(1)
-    return builder.as_markup() if builder.buttons else InlineKeyboardMarkup(inline_keyboard=[])
+    recent_book_markup = InlineKeyboardMarkup(row_width=1)
+    url_button = InlineKeyboardButton(text="↗️Url", url=url)
+    download_button = InlineKeyboardButton(text="📥 Download", url=download)
+
+    recent_book_markup.add(url_button, download_button)
+
+    return recent_book_markup
